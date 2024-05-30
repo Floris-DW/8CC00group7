a2e6561e909c3410c976f910fdd8e1579693563b
Keelpijpje <florisfw@gmail.com>
2024-05-30 14:49:31 +0200

    Initial commit.

---
+++ check_reqs.py
@@ -0,0 +1,19 @@
+import numpy
+import pandas
+import matplotlib
+import sklearn
+import rdkit
+import notebook
+import tensorflow
+import tensorflow.keras as keras
+
+print("numpy", numpy.__version__, numpy.__version__ == "1.26.4")
+print("pandas", pandas.__version__, pandas.__version__ == "2.2.2")
+print("matplotlib", matplotlib.__version__, matplotlib.__version__ ==
+      "3.9.0")
+print("sklearn", sklearn.__version__, sklearn.__version__ == "1.5.0")
+print("rdkit", rdkit.__version__, rdkit.__version__ == "2023.09.6")
+print("notebook", notebook.__version__, notebook.__version__ == "7.2.0")
+print("tensorflow", tensorflow.__version__, tensorflow.__version__ ==
+      "2.16.1")
+print("keras", keras.__version__, keras.__version__ == "3.3.3")
