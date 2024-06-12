8aef5ec33a1ae8c98273cc0ee040c22a227e7e54
Keelpijpje <florisfw@gmail.com>
2024-06-12 23:41:30 +0200

    Added code to train classifier models with customizability in the hyperparameters, the splitting of the dataset and cross-validation. It currently prints a bunch of metrics to evaluate.

--- ml.py
+++ ml.py
@@ -1,72 +1,209 @@
+from utils import descriptors_from_csv
 import numpy as np
 import pandas as pd
-from rdkit import Chem
-from rdkit.Chem import Descriptors
-from rdkit.Chem import AllChem
-from rdkit.Chem.Draw import IPythonConsole
-from rdkit.Chem import MACCSkeys
-from rdkit.ML.Descriptors import MoleculeDescriptors
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import (train_test_split, GridSearchCV,
+                                     StratifiedKFold)
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import (balanced_accuracy_score, confusion_matrix,
+                             roc_curve, roc_auc_score)
+from sklearn.ensemble import RandomForestClassifier
 
 
-def calculate_descriptors(data):
-    mols = [Chem.MolFromSmiles(smi) for smi in data['SMILES']]
-
-    # Calculate 2D Descriptors
-    # 2D Descriptors
-    desc_list = [x[0] for x in Descriptors._descList]
-    calc = MoleculeDescriptors.MolecularDescriptorCalculator(desc_list)
-    rdkit_desc = [calc.CalcDescriptors(m) for m in mols]
-
-    # Create 2d descriptor dataframe
-    desc_names = calc.GetDescriptorNames()
-    df_desc_2d = pd.DataFrame(rdkit_desc, index=data['SMILES'],
-                              columns=desc_names)
-
-    # Calculate Binary (Morgan) ECFP6 fingerprints
-    radius = 2  # 2 for similarity exploration, 3 for ML
-    n_bits = 1024  # 2048 is default, 1024 is also fine
-
-    # Calculate binary ECFP6 fingerprints:
-    fingerprints = [AllChem.GetMorganFingerprintAsBitVect(m,
-                    radius=radius, nBits=n_bits) for m in mols]
-
-    # Create ECFP6 fingerprint dataframe where each column represents a bit
-    fprint_cols = [f'Bit_{i}' for i in range(1, n_bits + 1)]
-    fprint_bits = [list(x) for x in fingerprints]
-    df_fprint = pd.DataFrame(fprint_bits, index=data['SMILES'],
-                             columns=fprint_cols)
-
-    # Calculate MACCS Keys
-    maccs_keys = np.array([MACCSkeys.GenMACCSKeys(m) for m in mols])
-    col_name = [f'feature_{i}' for i in range(1, len(maccs_keys[0]) + 1)]
-    # Create MACCS dataframe where each column corresponds to a MACCS feature (structural feature)
-    df_maccs = pd.DataFrame(data=maccs_keys, index=data['SMILES'], columns=col_name)
-
-    return df_desc_2d, df_fprint, df_maccs
+def train_model(x, y, model_type='RF', param_grid=None):
+    if model_type == 'RF':
+        clf = RandomForestClassifier()
+        if param_grid:
+            parameters = param_grid
+        else:
+            parameters = {'n_estimators': list(range(1, 5)),
+                          'max_depth': list(range(5, 20)),
+                          'min_samples_leaf': [1, 2],
+                          'min_samples_split': [2, 3]}
+    elif model_type == 'LR':
+        clf = LogisticRegression()
+        if param_grid:
+            parameters = param_grid
+        else:
+            parameters = {'penalty': ['l2'],
+                          'C': [1, 5, 10],
+                          'solver': ['liblinear'],
+                          'max_iter': [100000]}
+    else:
+        print("Invalid model type")
+        return -1
+    scorers = 'balanced_accuracy'
+    m = GridSearchCV(clf, param_grid=parameters, scoring=scorers)
+    m.fit(x, y)
+    #print(m.best_score_)
+    #print(m.best_params_)
+    return m.best_estimator_
 
 
-def get_training_test_split(df, targets, state=7):
-    y = targets
-    X_train, X_test, y_train, y_test = train_test_split(df, y, random_state=state)
-    return X_train, X_test, y_train, y_test
+def k_fold_training(x, y, model_type, random_state=7):
+    acc, auc = [], []  # balanced accuracies and ROC areas under curve
+    cv = StratifiedKFold(n_splits=n_splits,
+                         shuffle=True,
+                         random_state=random_state)
+    best_model, best_acc = None, 0
+    for train_i, test_i in cv.split(x, y):
+        x_train, x_test = x.iloc[train_i, :], x.iloc[test_i, :]
+        y_tr, y_te = y.iloc[train_i], y.iloc[test_i]
+
+        m = train_model(x_train, y_tr, model_type=model_type)
+        y_hat = m.predict(x_test)
+        cur_acc = balanced_accuracy_score(y_te, y_hat)
+        acc.append(cur_acc)
+        auc.append(roc_auc_score(y_te, m.predict_proba(x_test)[:, 1]))
+        # Store best model based on balanced accuracy:
+        if cur_acc > best_acc:
+            best_model, best_acc = m, cur_acc
+
+    return best_model, acc, auc
 
 
-data = pd.read_csv("data/tested_molecules.csv")
-y1 = data['PKM2_inhibition']
-y2 = data['ERK2_inhibition']
+if __name__ == '__main__':
+    data = pd.read_csv("data/tested_molecules.csv")
 
-df_2d_desc, df_fprint, df_maccs = calculate_descriptors(data)
-#print(df_2d_desc.shape)
-#print(df_fprint.shape)
-#print(df_maccs.shape)
-X = df_2d_desc
-model = LogisticRegression()
-model.fit(X, y1)
-pred_X = np.array(X.iloc[1115]).reshape(1, -1)
-print(model.predict(pred_X))
+    # -----------------------------------------------------------------
+    # CUSTOMIZE:
+    # -----------------------------------------------------------------
+    merge = True  # Whether you want to merge descriptor types
+    use_ind_test = True  # Whether you want to use independent test set
+    use_k_fold = False  # Whether you want to use k-fold cross-validation
+    m_type = "RF"  # model type: "RF" or "LR"
 
+    r_state = 7  # Random state for an attempt at reproducibility of results
+    test_size = 0.3  # Size of test set within Cross-Validation (validation set)
+    ind_test_size = 0.2  # Size of independent test set (if use_ind_test)
+    # k-value for k-fold cross-validation:
+    n_splits = 2  # 10 is generally recommended but it takes a long time
+    # customize hyperparameters: edit the param_grid in train_model()
+    # -----------------------------------------------------------------
 
-# https://stackoverflow.com/questions/70539674/train-neural-network-model-on-multiple-datasets
+    # Print settings
+    print(f"Settings: random_state={r_state}, test_size={test_size}",
+          end=", ")
+    if ind_test_size and use_k_fold:
+        print(f"ind_test_size={ind_test_size}, K-Folds={n_splits}")
+    elif ind_test_size:
+        print(f"ind_test_size={ind_test_size}")
+    elif use_k_fold:
+        print(f"K-Folds ={n_splits}")
+    else:
+        print()
+
+    desc = descriptors_from_csv(merged=merge)  # load descriptors
+    desc_names = ["2D Descriptors", "ECFP6 Fingerprints", "MACCS Keys"]
+    target_names = ["PKM2_inhibition", "ERK2_inhibition"]
+    for target_name in target_names:
+        target = data[target_name]
+        if merge:
+            if use_ind_test:
+                X, X_test_ind, Y, y_test_ind = (
+                    train_test_split(desc[0], target,
+                                     test_size=ind_test_size,
+                                     stratify=target,
+                                     random_state=r_state))
+            else:
+                X = desc[0]
+                Y = target
+
+            if use_k_fold:
+                # train
+                model, acc_l, auc_l = k_fold_training(
+                    X, Y, model_type=m_type, random_state=r_state)
+                # evaluate
+                print(f"{m_type} - {target_name} - Merged Descriptors "
+                      f"- KFolds: {n_splits}")
+                print(model.get_params())
+                print("bal_acc:", acc_l)
+                print(f"Mean bal_acc: {np.mean(acc_l)} std bal_acc: "
+                      f"{np.std(acc_l)}")
+                print("AUC:", auc_l)
+                print(f"Mean AUC: {np.mean(auc_l)} - std AUC: "
+                      f"{np.std(auc_l)}")
+                print()
+
+            else:  # no k-fold
+                # train
+                X_train, X_test, y_train, y_test = train_test_split(
+                    X, Y, random_state=r_state,
+                    test_size=test_size)
+                model = train_model(X_train, y_train, model_type=m_type)
+                # evaluate
+                y_pred = model.predict(X_test)
+                print(f"{m_type} - {target_name} - Merged Descriptors "
+                      f"- No KFold")
+                print(model.get_params())
+                print("acc:", balanced_accuracy_score(y_test, y_pred))
+                print("auc:", roc_auc_score(y_test, model.predict_proba(
+                                            X_test)[:, 1]))
+                # print((confusion_matrix(y_test.values, y_pred)))
+                print()
+            # Evaluate on independent test set
+            if use_ind_test:
+                y_pred = model.predict(X_test_ind)
+                print("Independent test set balanced accuracy_score:",
+                      balanced_accuracy_score(y_test_ind, y_pred))
+                print(confusion_matrix(y_test_ind.values, y_pred))
+                print()
+
+        else:  # not merged
+            for i in range(0, len(desc)):  # per descriptor
+                if use_ind_test:
+                    X, X_test_ind, Y, y_test_ind = (
+                        train_test_split(desc[i], target,
+                                         test_size=ind_test_size,
+                                         stratify=target,
+                                         random_state=r_state))
+                else:
+                    X, Y = desc[i], target
+
+                if use_k_fold:
+                    # train
+                    model, acc_l, auc_l = (
+                        k_fold_training(X, Y, model_type=m_type,
+                                        random_state=r_state))
+                    # evaluate
+                    print(f"{m_type} - {target_name} - {desc_names[i]} "
+                          f"- KFolds: {n_splits}")
+                    print(model.get_params())
+                    print("bal_acc:", acc_l)
+                    print(f"Mean bal_acc: {np.mean(acc_l)} std bal_acc:"
+                          f" {np.std(acc_l)}")
+                    print("AUC:", auc_l)
+                    print(f"Mean AUC: {np.mean(auc_l)} - std AUC: "
+                          f"{np.std(auc_l)}")
+                    print()
+
+                else:  # no k-fold
+                    # train
+                    X_train, X_test, y_train, y_test = train_test_split(
+                        X, Y, random_state=r_state,
+                        test_size=test_size)
+                    model = train_model(X_train, y_train,
+                                        model_type=m_type)
+                    # evaluate
+                    y_pred = model.predict(X_test)
+                    print(
+                        f"{m_type} - {target_name} - {desc_names[i]} - No KFold")
+                    print(model.get_params())
+                    print("acc:",
+                          balanced_accuracy_score(y_test, y_pred))
+                    print("auc:", roc_auc_score(y_test,
+                                                model.predict_proba(
+                                                    X_test)[
+                                                :, 1]))
+                    # print((confusion_matrix(y_test.values, y_pred)))
+                    print()
+
+                # evaluate independent test set
+                if use_ind_test:
+                    y_pred = model.predict(X_test_ind)
+                    print(f"{desc_names[i]} - Independent test set "
+                          "balanced accuracy_score:",
+                          balanced_accuracy_score(y_test_ind, y_pred))
+                    print(confusion_matrix(y_test_ind.values, y_pred))
+                    print()
+
 
