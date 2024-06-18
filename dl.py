58e5b6d0d86dde912350fe9dcefa08c9437a968c
HerrGeorg <glukanov2000@gmail.com>
2024-06-18 13:25:21 +0200

    pca and dl

--- dl.py
+++ dl.py
@@ -12,7 +12,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import GridSearchCV, StratifiedKFold, cross_val_score
 from sklearn.metrics import balanced_accuracy_score, confusion_matrix, accuracy_score, precision_score, recall_score, f1_score, make_scorer
-from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import Sequential, load_model
 from tensorflow.keras.layers import Dense, Dropout
 from scikeras.wrappers import KerasClassifier
 from tensorflow.keras.optimizers import Adam
@@ -20,6 +20,9 @@ from tensorflow.keras.callbacks import EarlyStopping
 from sklearn.utils.class_weight import compute_class_weight
 from imblearn.over_sampling import SMOTE
 from sklearn.ensemble import RandomForestClassifier
+from imblearn.ensemble import BalancedRandomForestClassifier
+
+
 
 
 def evaluate(y_test, y_pred, best_params):
@@ -54,41 +57,43 @@ df_fprint = pd.read_csv("data/cleaned_fingerprints.csv")
 df_maccs = pd.read_csv("data/cleaned_maccs_keys.csv")
 df_mqn = pd.read_csv("data/cleaned_mqn.csv")
 
+pca_2d_desc = pd.read_csv("data/pca_desc_2d.csv")
+pca_fprint = pd.read_csv("data/pca_fprint.csv")
+pca_maccs = pd.read_csv("data/pca_maccs.csv")
+pca_mqn = pd.read_csv("data/pca_mqn.csv")
 
 
-# df_combined = pd.concat([df_2d_desc, df_fprint, df_maccs, df_mqn], axis='columns')
-# df_combined.drop('SMILES', axis='columns', inplace=True)
+df_final = pd.concat([pca_mqn], axis='columns')
+# df_final.drop('SMILES', axis='columns', inplace=True)
+df_final = pd.DataFrame(MinMaxScaler().fit_transform(df_final), columns=df_final.columns)
 
-# df_combined_scaled = pd.DataFrame(MinMaxScaler().fit_transform(df_combined), columns=df_combined.columns)
-
-df_wo_fprint = pd.concat([df_2d_desc, df_maccs, df_mqn], axis='columns')
-df_wo_fprint.drop('SMILES', axis='columns', inplace=True)
-df_combined_scaled = pd.DataFrame(MinMaxScaler().fit_transform(df_wo_fprint), columns=df_wo_fprint.columns)
-
-
-# df_combined_target = y1.astype(str) + "_" + y2.astype(str)
 
 #######################################################################################################
 
-X_train_PKM2, X_test_PKM2, y_train_PKM2, y_test_PKM2 = train_test_split(df_combined_scaled, y_PKM2,
+X_train_PKM2, X_test_PKM2, y_train_PKM2, y_test_PKM2 = train_test_split(df_final, y_PKM2,
                                                             test_size=0.2, stratify=y_PKM2)
 
-X_train_ERK2, X_test_ERK2, y_train_ERK2, y_test_ERK2 = train_test_split(df_combined_scaled, y_ERK2, 
+X_train_ERK2, X_test_ERK2, y_train_ERK2, y_test_ERK2 = train_test_split(df_final, y_ERK2, 
                                                             test_size=0.2, stratify=y_ERK2)
 
+X_train_ERK2 = pd.concat([X_train_ERK2[y_train_ERK2==1], X_train_ERK2])
+y_train_ERK2 = pd.concat([y_train_ERK2[y_train_ERK2==1], y_train_ERK2])
 
-smote = SMOTE(random_state=42, sampling_strategy=0.1)
 
-X_train_ERK2_smote, y_train_ERK2_smote = smote.fit_resample(X_train_ERK2, y_train_ERK2)
-X_train_ERK2_smote = pd.concat([X_train_ERK2[y_train_ERK2==1], X_train_ERK2_smote])
-y_train_ERK2_smote = pd.concat([y_train_ERK2[y_train_ERK2==1], y_train_ERK2_smote])
+# smote = SMOTE(random_state=69, sampling_strategy=0.1)
 
-X_train_PKM2_smote, y_train_PKM2_smote = smote.fit_resample(X_train_PKM2, y_train_PKM2)
-X_train_PKM2_smote = pd.concat([X_train_PKM2[y_train_PKM2==1], X_train_PKM2_smote])
-y_train_PKM2_smote = pd.concat([y_train_PKM2[y_train_PKM2==1], y_train_PKM2_smote])
+# X_train_ERK2_smote, y_train_ERK2_smote = smote.fit_resample(X_train_ERK2, y_train_ERK2)
+# X_train_ERK2_smote = pd.concat([X_train_ERK2[y_train_ERK2==1], X_train_ERK2_smote])
+# y_train_ERK2_smote = pd.concat([y_train_ERK2[y_train_ERK2==1], y_train_ERK2_smote])
 
-X_train_ERK2, y_train_ERK2 = X_train_ERK2_smote.copy(), y_train_ERK2_smote.copy()
-X_train_PKM2, y_train_PKM2 = X_train_PKM2_smote.copy(), y_train_PKM2_smote.copy()
+
+# X_train_PKM2_smote, y_train_PKM2_smote = smote.fit_resample(X_train_PKM2, y_train_PKM2)
+# X_train_PKM2_smote = pd.concat([X_train_PKM2[y_train_PKM2==1], X_train_PKM2_smote])
+# y_train_PKM2_smote = pd.concat([y_train_PKM2[y_train_PKM2==1], y_train_PKM2_smote])
+
+
+# X_train_ERK2, y_train_ERK2 = X_train_ERK2_smote.copy(), y_train_ERK2_smote.copy()
+# X_train_PKM2, y_train_PKM2 = X_train_PKM2_smote.copy(), y_train_PKM2_smote.copy()
 
 
 
@@ -111,11 +116,11 @@ def create_mlp_model(input_dim, dropout_rate=0.25, init_mode='uniform'):
 input_dim = X_train_ERK2.shape[1]
 model = KerasClassifier(build_fn=create_mlp_model, input_dim=input_dim, verbose=1, 
                         dropout_rate = [0.2, 0.3, 0.4], batch_size = [32, 64], 
-                        init_mode = ['he_normal'])#, 'glorot_uniform', 'lecun_uniform'])
+                        init_mode = ['he_normal', 'lecun_uniform'])
 
 
 batch_size = [32, 64]
-dropout_rate = [0.3, 0.4, 0.5]
+dropout_rate = [0.2, 0.3, 0.4]
 init_mode = ['he_normal']#, 'glorot_uniform', 'lecun_uniform']
 param_grid = dict(batch_size=batch_size, dropout_rate=dropout_rate, init_mode=init_mode)
 
@@ -123,22 +128,28 @@ class_weights = compute_class_weight(class_weight='balanced',
                                      classes=np.array([0, 1]), y=y_train_ERK2)                       
 class_weight_dict = {0: class_weights[0], 1: class_weights[1]}  
 
-kfold = StratifiedKFold(n_splits=4, shuffle=True, random_state=42)
+
+kfold = StratifiedKFold(n_splits=4, shuffle=True, random_state=69)
 
 scorer = make_scorer(f1_score)
 
-grid = GridSearchCV(estimator=model, param_grid=param_grid, cv=kfold)#, scoring=scorer)
+grid = GridSearchCV(estimator=model, param_grid=param_grid, cv=kfold, scoring=scorer)
 
 grid_result = grid.fit(X_train_ERK2, y_train_ERK2, class_weight=class_weight_dict, 
                        callbacks=[EarlyStopping(monitor='val_loss', patience=3)])
 
-best_model = grid_result.best_estimator_#.model
+best_model = grid_result.best_estimator_.model_
 best_params = grid_result.best_params_
 
+# best_model.save('saved models/NN_mqn_1.keras')
+# loaded_model = load_model('saved models/NN_mqn_1.keras')
 
 y_pred_ERK2 = (best_model.predict(X_test_ERK2) > 0.5).astype("int32")
 evaluate(y_test_ERK2, y_pred_ERK2, best_params)
 
+
+# y_pred_ERK2 = (best_model.predict(X_train_ERK2) > 0.5).astype("int32")
+# evaluate(y_train_ERK2, y_pred_ERK2, best_params)
 
 
 
@@ -154,13 +165,13 @@ kfold = StratifiedKFold(n_splits=4, shuffle=True, random_state=42)
 
 scorer = make_scorer(balanced_accuracy_score)
 
-param_grid = {'n_estimators': [50, 100, 250],
-              'max_features': [100, 150, 'sqrt', 'log2'],
+param_grid = {'n_estimators': [50, 100],
+              'max_features': [100, 'sqrt', 'log2'],
               'max_depth': [2, 5, 10, 20],
               'min_samples_split': [2],
               'min_samples_leaf': [1]}
 
-rf = RandomForestClassifier(n_estimators=100, random_state=42, class_weight=class_weight_dict)
+rf = RandomForestClassifier(random_state=69, bootstrap=True, class_weight=class_weight_dict)
 
 
 grid_search = GridSearchCV(estimator=rf, param_grid=param_grid, scoring=scorer, 
@@ -171,31 +182,51 @@ grid_search.fit(X_train_ERK2, y_train_ERK2)
 best_rf = grid_search.best_estimator_
 best_params = grid_search.best_params_
 
-y_pred_ERK2 = best_rf.predict(X_test_ERK2)
-        
+
+y_pred_ERK2 = best_rf.predict(X_test_ERK2)        
 evaluate(y_test_ERK2, y_pred_ERK2, best_params)
 
 
 
 
+y_pred_ERK2 = (best_rf.predict(X_train_ERK2))
+evaluate(y_train_ERK2, y_pred_ERK2, best_params)
 
 
 ################################################################################################
 
 
-# y_pred_ERK2 = (best_model.predict(X_train_ERK2) > 0.5).astype("int32")
+kfold = StratifiedKFold(n_splits=4, shuffle=True, random_state=69)
 
-# evaluate(y_test_ERK2, y_pred_ERK2, best_params)
+scorer = make_scorer(balanced_accuracy_score)
+
+param_grid = {'n_estimators': [50, 100, 200],
+              'max_features': [100, 'sqrt', 'log2'],
+              'max_depth': [2, 5, 10, 20],
+              'min_samples_split': [2],
+              'min_samples_leaf': [1]}
+
+rf = BalancedRandomForestClassifier(random_state=69, bootstrap=True, 
+                                    replacement=True, sampling_strategy='all')
 
 
 
+grid_search = GridSearchCV(estimator=rf, param_grid=param_grid, scoring=scorer, 
+                           cv=kfold, n_jobs=-1, verbose=0)
+
+grid_search.fit(X_train_ERK2, y_train_ERK2)
+
+best_rf = grid_search.best_estimator_
+best_params = grid_search.best_params_
+
+
+y_pred_ERK2 = best_rf.predict(X_test_ERK2)        
+evaluate(y_test_ERK2, y_pred_ERK2, best_params)
 
 
 
-
-
-
-
+y_pred_ERK2 = (best_rf.predict(X_train_ERK2))
+evaluate(y_train_ERK2, y_pred_ERK2, best_params)
 
 
 
