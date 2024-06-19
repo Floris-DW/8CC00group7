06a4005f1e33f71558bbc466fd46f241f0edbeb6
HerrGeorg <glukanov2000@gmail.com>
2024-06-19 16:57:11 +0200

    Update untested_molecule_predictions.py

--- untested_molecule_predictions.py
+++ untested_molecule_predictions.py
@@ -17,11 +17,14 @@ df_fprint = df_fprint[df_fprints_block.columns]
 
 df_combined = pd.concat([df_mqn, df_fprint], axis='columns')
 
-best_model_ERK2 = load_model('saved models/NN_mqn_2.keras')
+best_model_ERK2 = load_model('saved models/NN_mqn_3.keras')
 best_model_PKM2 = load_model('saved models/NN_mqn+sfprint_3.keras')
 
 y_pred_ERK2 = (best_model_ERK2.predict(df_mqn) > 0.5).astype("int32")
 y_pred_PKM2 = (best_model_PKM2.predict(df_combined) > 0.5).astype("int32")
 
 
-data['']
+data['PKM2_inhibition'] = y_pred_PKM2
+data['ERK2_inhibition'] = y_pred_ERK2
+
+data.to_csv('untested_molecules_predicted.csv')
